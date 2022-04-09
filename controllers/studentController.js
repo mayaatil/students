74c3e27c1bc392ddf387ae1d111c10fffd1ac377
Maya Tillman <mayatillman17@gmail.com>
2022-04-09 13:29:24 -0500

    deploy again

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -6,6 +6,7 @@ const { Student, Course } = require("../models");
 const headCount = async () =>
   Student.aggregate()
     // Your code here
+    .count("studentCount")
     .then((numberOfStudents) => numberOfStudents);
 
 // Execute the aggregate method on the Student model and calculate the overall grade by using the $avg operator
@@ -13,14 +14,17 @@ const grade = async (studentId) =>
   Student.aggregate([
     // TODO: Ensure we include only the student who can match the given ObjectId using the $match operator
     {
-      // Your code here
+      $match: { _id: ObjectId(studentId) },
     },
     {
       $unwind: "$assignments",
     },
     // TODO: Group information for the student with the given ObjectId alongside an overall grade calculated using the $avg operator
     {
-      // Your code here
+      $group: {
+        _id: ObjectId(studentId),
+        overallGrade: { $avg: "$assignments.score" },
+      },
     },
   ]);
 
@@ -31,7 +35,7 @@ module.exports = {
       .then(async (students) => {
         const studentObj = {
           students,
-          // headCount: await headCount(),
+          headCount: await headCount(),
         };
         return res.json(studentObj);
       })
